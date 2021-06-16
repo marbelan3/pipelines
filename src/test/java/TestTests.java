be5fd4a361085fe6fde216e07fbc809f728effbc
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-16 16:46:00 +0300

    isBoolean

--- src/test/java/TestTests.java
+++ src/test/java/TestTests.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 public class TestTests {
     Logger logger = Logger.getLogger(getClass().getName());
     private static String choice_parameter_01;
-    private static boolean isBoolean;
+    private static String isBoolean;
 
     @BeforeClass
     public void beforeClass() {
@@ -17,7 +17,7 @@ public class TestTests {
         Logger.getLogger("hudson.plugins.git.GitStatus").setLevel(Level.SEVERE);
         Logger.getLogger("hudson.security.csrf.CrumbFilter").setLevel(Level.SEVERE);
         choice_parameter_01 = System.getenv("CHOICE_PARAMETER_01");
-        isBoolean = Boolean.getBoolean("isBoolean");
+        isBoolean = System.getenv("isBoolean");
         logger.info("choice_parameter_01" + choice_parameter_01);
     }
 
@@ -33,7 +33,7 @@ public class TestTests {
 
         logger.info("\n*****************************************\n" + isBoolean);
         logger.severe("\n*****************************************\n" + isBoolean);
-        if (isBoolean) {
+        if (Boolean.parseBoolean(isBoolean)) {
             System.out.println("123123123123123");
             throw new RuntimeException("\n*****************************************\n" +
                     "*****************************************\n" +
