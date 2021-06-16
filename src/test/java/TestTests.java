d5f07db25beed1cdc7af2a5948e13170dcd178fc
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-16 11:10:54 +0300

    getBoolean

--- src/test/java/TestTests.java
+++ src/test/java/TestTests.java
@@ -10,15 +10,29 @@ public class TestTests {
 
     Logger logger = LoggerFactory.getLogger(getClass().getName());
     private static String choice_parameter_01;
+    private static boolean isBooleanTrue;
 
     @BeforeClass
     public void beforeClass() {
         choice_parameter_01 = System.getenv("CHOICE_PARAMETER_01");
+        isBooleanTrue = Boolean.getBoolean("IS_BOOLEAN_TRUE");
         logger.info(choice_parameter_01);
     }
 
     @Test
     public void test() {
         Assert.assertEquals(choice_parameter_01, "ONE");
+    }
+
+    @Test
+    public void test2() {
+        if (isBooleanTrue) {
+            throw new RuntimeException("\n*****************************************\n" +
+                    "*****************************************\n" +
+                    "*****************************************\n" +
+                    "*****************************************\n" +
+                    "*****************************************\n" +
+                    "*****************************************");
+        }
     }
 }
