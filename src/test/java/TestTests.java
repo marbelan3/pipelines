4bd1e8994acac1f0ba01aa63a21588ae30d25136
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-16 16:47:50 +0300

    isBoolean

--- src/test/java/TestTests.java
+++ src/test/java/TestTests.java
@@ -9,22 +9,22 @@ import java.util.logging.Logger;
 public class TestTests {
     Logger logger = Logger.getLogger(getClass().getName());
     private static String choice_parameter_01;
-    private static String isBoolean;
+    private static boolean isBoolean;
 
     @BeforeClass
     public void beforeClass() {
 
-        Logger.getLogger("hudson.plugins.git.GitStatus").setLevel(Level.SEVERE);
-        Logger.getLogger("hudson.security.csrf.CrumbFilter").setLevel(Level.SEVERE);
+//        Logger.getLogger("hudson.plugins.git.GitStatus").setLevel(Level.SEVERE);
+//        Logger.getLogger("hudson.security.csrf.CrumbFilter").setLevel(Level.SEVERE);
         choice_parameter_01 = System.getenv("CHOICE_PARAMETER_01");
-        isBoolean = System.getenv("isBoolean");
+        isBoolean = Boolean.parseBoolean(System.getenv("isBoolean"));
         logger.info("choice_parameter_01" + choice_parameter_01);
     }
 
     @Test
     public void test() {
         logger.info("\n*****************************************\n" + choice_parameter_01);
-        logger.severe("\n*****************************************\n" + choice_parameter_01);
+//        logger.severe("\n*****************************************\n" + choice_parameter_01);
         Assert.assertEquals(choice_parameter_01, "ONE");
     }
 
@@ -33,7 +33,7 @@ public class TestTests {
 
         logger.info("\n*****************************************\n" + isBoolean);
         logger.severe("\n*****************************************\n" + isBoolean);
-        if (Boolean.parseBoolean(isBoolean)) {
+        if (isBoolean) {
             System.out.println("123123123123123");
             throw new RuntimeException("\n*****************************************\n" +
                     "*****************************************\n" +
