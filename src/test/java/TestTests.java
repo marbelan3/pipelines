9766106486d04f333cdf778b01c1526587d27a0b
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-23 00:13:45 +0300

    123

--- src/test/java/TestTests.java
+++ src/test/java/TestTests.java
@@ -10,73 +10,32 @@ public class TestTests {
 
     Logger logger = Logger.getLogger(getClass().getName());
     private static String choice_parameter_01;
-    private static boolean isBoolean;
 
     @BeforeClass
     public void beforeClass() {
         choice_parameter_01 = System.getenv("CHOICE_PARAMETER_01");
         milliseconds = System.getProperty("milliseconds");
-        isBoolean = Boolean.parseBoolean(System.getenv("isBoolean"));
+        enabled = Boolean.parseBoolean(System.getenv("isBoolean"));
         logger.info("choice_parameter_01" + choice_parameter_01);
     }
 
     @Test
     public void test() {
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("***************" + enabled + "***********");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+        System.out.println("****************************************");
+
         logger.info("\n*****************************************\n" + choice_parameter_01);
         Assert.assertEquals(choice_parameter_01, "ONE");
     }
 
-    //    @Parameters({"milliseconds"})
-    @Test
-    public void test2() {
-        System.out.println(milliseconds);
-        System.out.println(milliseconds);
-        System.out.println(milliseconds);
-        logger.info("\n*****************************************\n" + milliseconds);
-        logger.info("\n*****************************************\n" + milliseconds);
-        logger.info("\n*****************************************\n" + isBoolean);
-        if (isBoolean) {
-            logger.info(
-                    "\n******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n" +
-                            "******************** S U C C E S S  *********************\n");
-        }
-        if (enabled) {
-            logger.info(
-                    "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n" +
-                            "\n******************** enabled - true  *********************\n");
-        }
-        Assert.assertTrue(isBoolean, "The boolean parameter is 'F A L S E'");
-    }
 
-
-//    @Test
-//    @Parameters({"milliseconds"})
-//    public void browserTest(int millisec) {
-//        Selenide.open("https://google.com");
-//        if (enabled) {
-//            logger.info(
-//                    "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n" +
-//                            "\n******************** enabled - true  *********************\n");
-//        }
-//
-//        Selenide.sleep(millisec);
-//        logger.info("Browser has been wait a " + millisec + " milliseconds");
-//    }
 }
