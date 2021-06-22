c45af7ab310eb3d32087607ee2f1858f7c55e9b0
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-22 20:43:26 +0300

    123

--- src/test/java/TestTests.java
+++ src/test/java/TestTests.java
@@ -1,11 +1,12 @@
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.util.logging.Logger;
 
 
 public class TestTests {
     public static boolean enabled = System.getProperty("enabled") != null && System.getProperty("enabled").equalsIgnoreCase("true");
-    public static int milliseconds;
+    public static String milliseconds;
 
     Logger logger = Logger.getLogger(getClass().getName());
     private static String choice_parameter_01;
@@ -14,7 +15,7 @@ public class TestTests {
     @BeforeClass
     public void beforeClass() {
         choice_parameter_01 = System.getenv("CHOICE_PARAMETER_01");
-        milliseconds = Integer.parseInt(System.getProperty("milliseconds"));
+        milliseconds = System.getProperty("milliseconds");
         isBoolean = Boolean.parseBoolean(System.getenv("isBoolean"));
         logger.info("choice_parameter_01" + choice_parameter_01);
     }
@@ -29,6 +30,10 @@ public class TestTests {
     @Test
     public void test2() {
         System.out.println(milliseconds);
+        System.out.println(milliseconds);
+        System.out.println(milliseconds);
+        logger.info("\n*****************************************\n" + milliseconds);
+        logger.info("\n*****************************************\n" + milliseconds);
         logger.info("\n*****************************************\n" + isBoolean);
         if (isBoolean) {
             logger.info(
