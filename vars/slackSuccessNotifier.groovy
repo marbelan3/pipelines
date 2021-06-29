b4c46916248cfed51ccd392d7b1a778359e7540d
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-30 00:18:38 +0300

    slackSuccessNotifier

--- vars/slackSuccessNotifier.groovy
+++ vars/slackSuccessNotifier.groovy
@@ -2,6 +2,6 @@
 
 def call() {
     slackSend channel: '#tests-ui-tests-api',
-            color: '#warning',
+            color: '#BADA55',
             message: "The pipeline ${currentBuild.fullDisplayName} completed successfully!!!!"
 }
