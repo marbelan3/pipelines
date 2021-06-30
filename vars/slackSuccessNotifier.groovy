943b204792185724389596dfa3f62c245cfb7a0d
Serhii.Danevych@aerlingus.com <Marbels1988>
2021-06-30 10:10:27 +0300

    slackResponse

--- vars/slackSuccessNotifier.groovy
+++ vars/slackSuccessNotifier.groovy
@@ -4,4 +4,7 @@ def call() {
     slackSend channel: '#tests-ui-tests-api',
             color: '#BADA55',
             message: "The pipeline ${currentBuild.fullDisplayName} completed successfully!!!!"
+    def slackResponse = slackSend(channel: "cool-threads", message: "Here is the primary message")
+    slackSend(channel: slackResponse.threadId, message: "Thread reply #1")
+    slackSend(channel: slackResponse.threadId, message: "Thread reply #2")
 }
